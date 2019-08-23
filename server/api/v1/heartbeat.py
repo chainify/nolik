d248edb653983826fb5085752c75aa69912b29d7
amrbz <aboziev@gmail.com>
2019-08-23 08:50:27 +0300

    pdate of Redis config

--- server/api/v1/heartbeat.py
+++ server/api/v1/heartbeat.py
@@ -7,27 +7,26 @@ import psycopg2
 import configparser
 from .threads import get_threads
 from .cdms import get_cdms
+
 import redis
+# from redis.connection import ConnectionPool
 
 config = configparser.ConfigParser()
 config.read('config.ini')
 
 heartbeat = Blueprint('heartbeat_v1', url_prefix='/heartbeat')
 
+
 class HeartBeat(HTTPMethodView):
     @staticmethod
     def post(request):
         public_key = request.form['publicKey'][0]
         last_tx_id = request.form['lastTxId'][0] if 'lastTxId' in request.form else None
-        
-        pool = redis.ConnectionPool(
-            host=os.environ['REDIS_HOST'],
-            port=os.environ['REDIS_PORT'],
-            password=os.environ['REDIS_PASSWORD'] or None)
-        r = redis.Redis(connection_pool=pool)
 
+        client = redis.Redis.from_url(os.environ['REDIS_URL'])
+        r = redis.Redis(connection_pool=client.connection_pool)
         pipe = r.pipeline()
-        pipe.set(public_key, last_tx_id or 'NULL').expire(public_key, 2).execute()
+        pipe.set(public_key, 'True').expire(public_key, 4).execute()
 
         data = {
             'threads': get_threads(public_key, last_tx_id),
