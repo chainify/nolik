242705ac7a5806da9f535b5b5ccc16fa0d792ecd
Amir Boziev <aboziev@gmail.com>
2022-09-03 12:13:30 +0300

    Switch to CLI

--- parser/api/v1/__init__.py
+++
@@ -1,6 +0,0 @@
-from sanic import Blueprint
-from .parser import parser
-
-api_v1 = Blueprint.group(parser, url_prefix='/v1')
-
-
--- server/api/__init__.py
+++
@@ -1,5 +0,0 @@
-from sanic import Blueprint
-
-from .v1 import api_v1
-
-api_v1 = Blueprint.group(api_v1, url_prefix='/api')
