58fe75006367d09d4b66ce67ed0480ebcd13fccd
Kirill Konevets <kkonevets@gmail.com>
2023-02-16 06:26:45 +0300

    wrap proc macro with a parent directory

--- cli/nolik-cypher/src/lib.rs
+++ cli/nolik-cypher/src/lib.rs
@@ -1,10 +1,2 @@
-//! This crate implements the macro for `cypher` and should not be used directly.
-
-mod impl_derive;
-
-use proc_macro::TokenStream;
-
-#[proc_macro_derive(Cypher)]
-pub fn derive_cypher(input: TokenStream) -> TokenStream {
-	impl_derive::impl_derive_cypher(input)
-}
+#[doc(inline)]
+pub use cypher_macro::Cypher;
